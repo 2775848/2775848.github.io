cd68d987b2b8f11d73ec1897687e08ca12f2fab1
2775848 <9262775848@mail.ru>
2020-12-21 20:48:42 +0300

    add 02

--- 02/js/index.js
+++ 02/js/index.js
@@ -1,3 +1,29 @@
+/**
+ * NodeList.prototype.forEach() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+ */
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 function scrollTo(element) {
    element.scrollIntoView({block: "start", behavior: "smooth"});
 }
@@ -23,3 +49,55 @@ mMenuBurger.addEventListener('click', () => {
    mBody.classList.toggle('lock');
 });
 
+*/
+
+/* показать доп 3 карточки объектов */ 
+let addCardsBtn = document.querySelector('.add-cards-btn')
+let cardHidden = document.querySelectorAll('.card-link.hidden');
+
+addCardsBtn.addEventListener('click', () => { 
+   cardHidden.forEach( item => {
+      item.classList.remove('hidden')
+   })
+});
+/* показа/скрыть 3 доп.опции фильтра */
+let filterOptionsAddBtn = document.querySelector('.filter-options-add-btn');
+let hiddenOptions = document.querySelector('.filter-options-add.hidden');
+
+filterOptionsAddBtn.addEventListener('click', () => {
+   hiddenOptions.classList.toggle('hidden');
+   if (filterOptionsAddBtn.innerHTML == "Показать ещё") {
+      filterOptionsAddBtn.innerHTML = "Скрыть"
+   } else {filterOptionsAddBtn.innerHTML = "Показать ещё"}
+});
+
+/* скрыть/показать фильтр по клику на заголовок   */
+let filterName = document.querySelectorAll('.filter__name');
+filterName.forEach( item => {
+   item.addEventListener('click', () => {
+      item.nextElementSibling.classList.toggle('hidden')
+      item.classList.toggle('rotate');
+   })
+});
+
+/* обработка нажатий на фильтр Метро */
+let filterMetroAny = document.querySelector('#filter-metro-any')
+let filterMetroRow = document.querySelectorAll('.filter-metro__row > input')
+
+//--  кнопка "Любая" при нажатии всегда активна и сбрасывает остальные --
+filterMetroAny.addEventListener('click', () => {
+   filterMetroAny.checked = true;
+   filterMetroRow.forEach ( item => item.checked = false );
+});
+//--  остальные кнопки фильтра Метро --
+filterMetroRow.forEach( item => {
+   item.addEventListener('click', () => {
+      //-- проверяем есть ли хоть один элемент cheked вкл/выкл кнопку Любая
+      if ( Array.from(filterMetroRow).some( i => i.checked ) ) {
+         filterMetroAny.checked = false;
+      } else { 
+         filterMetroAny.checked = true; 
+      }
+   });
+});
+ /* ----- */
